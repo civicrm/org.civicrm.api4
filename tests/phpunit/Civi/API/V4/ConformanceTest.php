110e18300d1744451bf9fdd89d3a7513e15b0f7f
Erich Schulz <erichbschulz@gmail.com>
2017-02-26 10:40:46 +1000

    augment generic CRUD test
    still need to figure out a generic "UPDATE test metadata pattern"
    
    also return profile from delete needs confirming

--- tests/phpunit/Civi/API/V4/ConformanceTest.php
+++ tests/phpunit/Civi/API/V4/ConformanceTest.php
@@ -32,13 +32,14 @@ class ConformanceTest extends UnitTestCase  {
   }
 
   public function testConformance() {
+    // get list of all the entities we know about and loop over them:
     $entities = Entity::get()
       ->setCheckPermissions(FALSE)
       ->execute();
     foreach ($entities as $entity) {
-      $fullclass = 'Civi\Api4\\' . $entity;
+      $entity_class = 'Civi\Api4\\' . $entity;
       $this->report("## Testing $entity");
-      $actions = $fullclass::getActions()
+      $actions = $entity_class::getActions()
         ->setCheckPermissions(FALSE)
         ->execute()
         ->indexBy('name');
@@ -47,14 +48,38 @@ class ConformanceTest extends UnitTestCase  {
 
 
       if ($entity != 'Entity') {
+        // CREATE ////////////////////
         $dummy = $this->sample(array('type' => $entity))['sample_params'];
-        echo '$dummy: '.json_encode($dummy,JSON_PRETTY_PRINT)."\n";
-        $create_result = $fullclass::create()
+        $create_result = $entity_class::create()
           ->setValues($dummy)
           ->setCheckPermissions(FALSE)
           ->execute();
-        echo 'create_result: '.json_encode($create_result,JSON_PRETTY_PRINT)."\n";
+        $this->assertArrayHasKey('id', $create_result, "create missing ID");
+        $id = $create_result['id'];
+        $this->assertGreaterThanOrEqual(1, $id, "$entity ID not positive");
+        // RETRIEVE ////////////////////
+        $get_result = $entity_class::get()
+          ->setCheckPermissions(FALSE)
+          ->addClause(array('id', '=', $id))
+          ->execute();
+        $this->assertEquals(1, count($get_result),
+          "failed to get single fresh $entity");
+        // UPDATE ////////////////////
 
+        // DELETE ////////////////////
+        $delete_result = $entity_class::delete()
+          ->setCheckPermissions(FALSE)
+          ->addClause(array('id', '=', $id))
+          ->execute();
+        // should get back an array of deleted id:
+        $this->assertEquals(array($id), (array)$delete_result,
+          "unexpected delete result from $entity");
+        $get_deleted_result = $entity_class::get()
+          ->setCheckPermissions(FALSE)
+          ->addClause(array('id', '=', $id))
+          ->execute();
+        $this->assertEquals(0, count($get_deleted_result),
+          "still getting back data after delete of $entity");
       }
     }
   }
