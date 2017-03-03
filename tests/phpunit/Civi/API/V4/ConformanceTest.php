667e4a66a056971eb78e3fa0de9c24db15bcfe4b
Erich <erichbschulz@gmail.com>
2017-03-03 16:39:03 +1000

    lint file and tone down header comments

--- tests/phpunit/Civi/API/V4/ConformanceTest.php
+++ tests/phpunit/Civi/API/V4/ConformanceTest.php
@@ -10,17 +10,16 @@ use Civi\Test\TransactionalInterface;
 /**
  * @group headless
  */
-//class ParticipantTest extends \PHPUnit_Framework_TestCase implements HeadlessInterface, TransactionalInterface {
-class ConformanceTest extends UnitTestCase  {
+class ConformanceTest extends UnitTestCase {
 
- /**
+  /**
    * Set up baseline for testing
    */
   public function setUp() {
     parent::setUp();
   }
 
- /**
+  /**
    * Tears down the fixture, for example, closes a network connection.
    *
    * This method is called after a test is executed.
@@ -45,16 +44,17 @@ class ConformanceTest extends UnitTestCase  {
         ->setCheckPermissions(FALSE)
         ->execute()
         ->indexBy('name');
-      $this->report("Actions: \n".json_encode(
-        array_keys((array)$actions),JSON_PRETTY_PRINT));
+      $this->report("Actions: \n" . json_encode(
+        array_keys((array) $actions), JSON_PRETTY_PRINT));
 
       if ($entity != 'Entity') {
-        // FIELDS ////////////////////
+        // fields
         $fields = $entity_class::getFields()
           ->setCheckPermissions(FALSE)
-          ->execute()->indexBy('name');
-        $this->report("Fields: \n".json_encode(
-          (array)$fields, JSON_PRETTY_PRINT));
+          ->execute()
+          ->indexBy('name');
+        $this->report("Fields: \n" . json_encode(
+          (array) $fields, JSON_PRETTY_PRINT));
         $this->assertArraySubset(
           array('type' => 1, 'required' => TRUE),
           $fields['id'],
@@ -63,7 +63,7 @@ class ConformanceTest extends UnitTestCase  {
           array('type' => 1, 'required' => TRUE),
           $fields['id'],
           "$entity fields missing required ID field of proper type");
-        // CREATE ////////////////////
+        // create
         $dummy = $this->sample(array('type' => $entity))['sample_params'];
         $create_result = $entity_class::create()
           ->setValues($dummy)
@@ -72,22 +72,22 @@ class ConformanceTest extends UnitTestCase  {
         $this->assertArrayHasKey('id', $create_result, "create missing ID");
         $id = $create_result['id'];
         $this->assertGreaterThanOrEqual(1, $id, "$entity ID not positive");
-        // RETRIEVE ////////////////////
+        // retrieve
         $get_result = $entity_class::get()
           ->setCheckPermissions(FALSE)
           ->addClause(array('id', '=', $id))
           ->execute();
         $this->assertEquals(1, count($get_result),
           "failed to get single fresh $entity");
-        // UPDATE ////////////////////
+        // update
 
-        // DELETE ////////////////////
+        // delete
         $delete_result = $entity_class::delete()
           ->setCheckPermissions(FALSE)
           ->addClause(array('id', '=', $id))
           ->execute();
         // should get back an array of deleted id:
-        $this->assertEquals(array($id), (array)$delete_result,
+        $this->assertEquals(array($id), (array) $delete_result,
           "unexpected delete result from $entity");
         $get_deleted_result = $entity_class::get()
           ->setCheckPermissions(FALSE)
