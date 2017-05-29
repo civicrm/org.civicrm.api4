707aa574df86c8723b8fd1f1c684d1c8336215fd
mickadoo <michaeldevery@gmail.com>
2017-05-29 14:19:53 +0100

    #37 Add CustomField and CustomGroup classes

---
+++ Civi/Api4/CustomGroup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Civi\Api4;
+
+use Civi\API\V4\Entity;
+use Civi\API\V4\Entity\CustomGroup\Create;
+
+/**
+ * @method static Create create
+ */
+class CustomGroup extends Entity {
+
+}
