ec308e79b655d2156957d9c2301ec2348feb709c
mickadoo <michaeldevery@gmail.com>
2017-06-06 14:04:04 +0100

    #62 Remove obsolete docblock

--- Civi/Api4/CustomField.php
+++ Civi/Api4/CustomField.php
@@ -3,11 +3,7 @@
 namespace Civi\Api4;
 
 use Civi\API\V4\Entity;
-use Civi\API\V4\Entity\CustomField\Create;
 
-/**
- * @method static Create create
- */
 class CustomField extends Entity {
 
 }
--- Civi/Api4/CustomGroup.php
+++ Civi/Api4/CustomGroup.php
@@ -3,11 +3,7 @@
 namespace Civi\Api4;
 
 use Civi\API\V4\Entity;
-use Civi\API\V4\Entity\CustomGroup\Create;
 
-/**
- * @method static Create create
- */
 class CustomGroup extends Entity {
 
 }
