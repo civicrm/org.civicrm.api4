6ec3b77d47df0d9a464aad63d72900a2a2665057
mickadoo <michaeldevery@gmail.com>
2017-06-27 16:00:29 +0100

    #54 Add option value joinable

---
+++ Civi/API/Service/Schema/Joinable/OptionValueJoinable.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Civi\API\Service\Schema\Joinable;
+
+class OptionValueJoinable extends Joinable {
+  /**
+   * @var string
+   */
+  protected $optionGroupName;
+
+  /**
+   * @param string $optionGroupName
+   * @param string|null $alias
+   */
+  public function __construct($optionGroupName, $alias = NULL) {
+    $this->optionGroupName = $optionGroupName;
+    $optionValueTable = 'civicrm_option_value';
+
+    // default join alias to option group name, e.g. activity_type
+    if (!$alias) {
+      $alias = $optionGroupName;
+    }
+
+    parent::__construct($optionValueTable, 'value', $alias);
+
+    $subSelect = 'SELECT id FROM civicrm_option_group WHERE name = "%s"';
+    $subQuery = sprintf($subSelect, $optionGroupName);
+    $condition = sprintf('%s.option_group_id = (%s)', $alias, $subQuery);
+    $this->addCondition($condition);
+  }
+}
