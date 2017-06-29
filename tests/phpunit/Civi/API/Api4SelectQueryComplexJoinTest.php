723c34046954decc4d4730defda196ed53e14323
mickadoo <michaeldevery@gmail.com>
2017-06-29 21:04:10 +0100

    #54 Add test for auto-joining and selection including use of subscriber

---
+++ tests/phpunit/Civi/API/Api4SelectQueryComplexJoinTest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace phpunit\Civi\API;
+
+use Civi\API\Api4SelectQuery;
+use Civi\API\V4\UnitTestCase;
+use Civi\TestDataLoaderTrait;
+
+/**
+ * @group headless
+ */
+class Api4SelectQueryComplexJoinTest extends UnitTestCase {
+
+  use TestDataLoaderTrait;
+
+  public function setUpHeadless() {
+    $relatedTables = array(
+      'civicrm_contact',
+      'civicrm_option_group',
+      'civicrm_option_value',
+      'civicrm_activity',
+      'civicrm_activity_contact',
+    );
+    $this->cleanup(array('tablesToTruncate' => $relatedTables));
+    $this->loadDataSet('SingleContactWithPhoneEmailAndActivities');
+    return parent::setUpHeadless();
+  }
+
+  public function testWithComplexRelatedEntitySelect() {
+    $query = new Api4SelectQuery('Contact', FALSE);
+    $query->select[] = 'id';
+    $query->select[] = 'display_name';
+    $query->select[] = 'phone.phone';
+    $query->select[] = 'email.email';
+    $query->select[] = 'email.location_type.name';
+    $query->select[] = 'created_activity.contact_id';
+    $query->select[] = 'created_activity.activity.subject';
+    $query->select[] = 'created_activity.activity.activity_type.name';
+    $results = $query->run();
+
+    $testActivities = [
+      $this->getReference('test_activity_1'),
+      $this->getReference('test_activity_2'),
+    ];
+    $activitySubjects = array_column($testActivities, 'subject');
+
+    $this->assertCount(1, $results);
+    $firstResult = array_shift($results);
+    $this->assertArrayHasKey('created_activity', $firstResult);
+    $firstCreatedActivity = array_shift($firstResult['created_activity']);
+    $this->assertArrayHasKey('activity', $firstCreatedActivity);
+    $firstActivity = array_shift($firstCreatedActivity['activity']);
+    $this->assertContains($firstActivity['subject'], $activitySubjects);
+    $this->assertArrayHasKey('activity_type', $firstActivity);
+    $activityType = array_shift($firstActivity['activity_type']);
+    $this->assertArrayHasKey('name', $activityType);
+  }
+}
