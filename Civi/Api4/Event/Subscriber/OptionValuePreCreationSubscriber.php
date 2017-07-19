1e8beb216dd7214566085085c8ff8f5f4a93e6ea
mickadoo <michaeldevery@gmail.com>
2017-07-19 10:35:45 +0100

    Add constructor dependency on subscriber

--- Civi/Api4/Event/Subscriber/OptionValuePreCreationSubscriber.php
+++ Civi/Api4/Event/Subscriber/OptionValuePreCreationSubscriber.php
@@ -3,9 +3,22 @@
 namespace Civi\Api4\Event\Subscriber;
 
 use Civi\Api4\Api\OptionGroupApi;
+use Civi\Api4\GetParameterBag;
 use Civi\Api4\Request;
 
 class OptionValuePreCreationSubscriber extends PreCreationSubscriber {
+
+  /**
+   * @var OptionGroupApi
+   */
+  protected $optionGroupApi;
+
+  /**
+   * @param OptionGroupApi $optionGroupApi
+   */
+  public function __construct(OptionGroupApi $optionGroupApi) {
+    $this->optionGroupApi = $optionGroupApi;
+  }
 
   /**
    * @inheritdoc
@@ -32,11 +45,10 @@ class OptionValuePreCreationSubscriber extends PreCreationSubscriber {
       return;
     }
 
-    $optionGroup = OptionGroupApi::get() // todo make dependency
-      ->setCheckPermissions(FALSE)
-      ->addSelect('id')
-      ->addWhere('name', '=', $optionGroupName)
-      ->execute();
+    $params = new GetParameterBag();
+    $params->addSelect('id');
+    $params->addWhere('name', '=', $optionGroupName);
+    $optionGroup = $this->optionGroupApi->get($params);
 
     if ($optionGroup->count() !== 1) {
       throw new \Exception('Option group name must match only a single group');
