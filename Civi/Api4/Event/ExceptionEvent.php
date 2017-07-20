3413ed218357a2b9ee889dd37ed91dd09f7b5d60
mickadoo <michaeldevery@gmail.com>
2017-07-20 18:28:27 +0100

    Cleanup exception subscriber, add context to event

--- Civi/Api4/Event/ExceptionEvent.php
+++ Civi/Api4/Event/ExceptionEvent.php
@@ -2,6 +2,7 @@
 
 namespace Civi\Api4\Event;
 
+use Civi\Api4\Response;
 use Symfony\Component\EventDispatcher\Event;
 
 class ExceptionEvent extends Event {
@@ -9,14 +10,27 @@ class ExceptionEvent extends Event {
   /**
    * @var \Exception
    */
-  private $exception;
+  protected $exception;
+
+  /**
+   * @var bool
+   */
+  protected $isDebug;
+
+  /**
+   * @var Response
+   */
+  protected $response;
 
   /**
    * @param \Exception $exception
    *   The exception which arose while processing the API request.
+   * @param bool $isDebug
+   *   Whether debug mode is on or not.
    */
-  public function __construct($exception) {
+  public function __construct($exception, $isDebug = FALSE) {
     $this->exception = $exception;
+    $this->isDebug = $isDebug;
   }
 
   /**
@@ -24,6 +38,38 @@ class ExceptionEvent extends Event {
    */
   public function getException() {
     return $this->exception;
+  }
+
+  /**
+   * @return Response
+   */
+  public function getResponse() {
+    return $this->response;
+  }
+
+  /**
+   * @return bool
+   */
+  public function hasResponse() {
+    return NULL !== $this->getResponse();
+  }
+
+  /**
+   * @param Response $response
+   *
+   * @return $this
+   */
+  public function setResponse($response) {
+    $this->response = $response;
+
+    return $this;
+  }
+
+  /**
+   * @return bool
+   */
+  public function isDebug() {
+    return $this->isDebug;
   }
 
 }
