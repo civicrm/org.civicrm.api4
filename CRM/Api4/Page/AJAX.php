8d5b2f6606d2ae0e9766be7ad8ecccfe6ed5b2f8
Coleman Watts <coleman@civicrm.org>
2017-11-28 17:15:16 -0500

    Support multiple ajax calls

--- CRM/Api4/Page/AJAX.php
+++ CRM/Api4/Page/AJAX.php
@@ -2,34 +2,54 @@
 
 class CRM_Api4_Page_AJAX extends CRM_Core_Page {
 
+  /**
+   * Handler for api4 ajax requests
+   */
   public function run() {
-    $entity = $this->urlPath[3];
-    $action = $this->urlPath[4];
-    $params = CRM_Utils_Request::retrieve('params', 'String');
-    if ($params) {
-      $params = json_decode($params, TRUE);
-    }
-    else {
-      $params = array();
-    }
-    CRM_Utils_System::setHttpHeader('Content-Type', 'application/json');
     try {
-      $response = $this->execute($entity, $action, $params);
+      // Call multiple
+      if (empty($this->urlPath[3])) {
+        $calls = CRM_Utils_Request::retrieve('calls', 'String', CRM_Core_DAO::$_nullObject, TRUE, NULL, 'REQUEST', TRUE);
+        $calls = json_decode($calls, TRUE);
+        $response = array();
+        foreach ($calls as $index => $call) {
+          $response[$index] = $this->execute($call[0], $call[1], CRM_Utils_Array::value(2, $call, array()));
+        }
+      }
+      // Call single
+      else {
+        $entity = $this->urlPath[3];
+        $action = $this->urlPath[4];
+        $params = CRM_Utils_Request::retrieve('params', 'String');
+        $params = $params ? json_decode($params, TRUE) : array();
+        $response = $this->execute($entity, $action, $params);
+      }
     }
     catch (Exception $e) {
       http_response_code(500);
       $response = array(
         'error_code' => $e->getCode(),
-        'error_message' => $e->getMessage(),
       );
       if (CRM_Core_Permission::check('view debug output')) {
-        $response['backtrace'] = $e->getTrace();
+        $response['error_message'] = $e->getMessage();
+        if (CRM_Core_BAO_Setting::getItem(NULL, 'backtrace')) {
+          $response['backtrace'] = $e->getTrace();
+        }
       }
     }
+    CRM_Utils_System::setHttpHeader('Content-Type', 'application/json');
     echo json_encode($response);
     CRM_Utils_System::civiExit();
   }
 
+  /**
+   * Run api call & prepare result for json encoding
+   *
+   * @param $entity
+   * @param $action
+   * @param $params
+   * @return array
+   */
   protected function execute($entity, $action, $params) {
     $params['checkPermissions'] = TRUE;
     $result = civicrm_api4($entity, $action, $params);
