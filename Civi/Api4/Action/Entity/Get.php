efea5726a28795f0720c5da06477f6058864d282
Vasyl Sokolik <vsokolyk@gmail.com>
2018-05-02 16:47:55 +0300

    updated drupal code style updated drupal style added EntityUtil class fixed prs style

--- Civi/Api4/Action/Entity/Get.php
+++ Civi/Api4/Action/Entity/Get.php
@@ -43,19 +43,19 @@ class Get extends AbstractAction {
    */
   public function _run(Result $result) {
     $entities = [];
-    foreach (explode(PATH_SEPARATOR, get_include_path()) as $path) {
+    foreach (\explode(PATH_SEPARATOR, \get_include_path()) as $path) {
       $dir = \CRM_Utils_File::addTrailingSlash($path) . 'Civi/Api4';
-      if (is_dir($dir)) {
-        foreach (glob("$dir/*.php") as $file) {
+      if (\is_dir($dir)) {
+        foreach (\glob("${dir}/*.php") as $file) {
           $matches = [];
-          preg_match('/(\w*).php/', $file, $matches);
+          \preg_match('/(\w*).php/', $file, $matches);
           $entities[$matches[1]] = $matches[1];
         }
       }
     }
-    $entities = array_values($entities);
-    if (in_array('BaseEntity', $entities)) {
-      unset($entities[array_search('BaseEntity', $entities)]);
+    $entities = \array_values($entities);
+    if (\in_array('BaseEntity', $entities)) {
+      unset($entities[\array_search('BaseEntity', $entities)]);
     }
     $result->exchangeArray($entities);
   }
