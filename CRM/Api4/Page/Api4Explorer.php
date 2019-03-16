e560d83610b55ca7d1b129d299ad7b1311973c3f
Coleman Watts <coleman@civicrm.org>
2019-03-15 22:27:34 -0400

    Short array syntax

--- CRM/Api4/Page/Api4Explorer.php
+++ CRM/Api4/Page/Api4Explorer.php
@@ -5,11 +5,11 @@ class CRM_Api4_Page_Api4Explorer extends CRM_Core_Page {
 
   public function run() {
     $loader = new \Civi\Angular\AngularLoader();
-    $loader->setModules(array('api4Explorer'));
+    $loader->setModules(['api4Explorer']);
     $loader->setPageName('civicrm/api4');
-    $loader->useApp(array(
+    $loader->useApp([
       'defaultRoute' => '/explorer',
-    ));
+    ]);
     $loader->load();
     CRM_Utils_System::setTitle('CiviCRM');
     parent::run();
