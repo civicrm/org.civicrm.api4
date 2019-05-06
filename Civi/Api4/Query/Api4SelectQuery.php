08e40412c30aa3cfdf1ba5fef346317caae2af35
Coleman Watts <coleman@civicrm.org>
2019-05-06 19:56:30 -0400

    Better error handling of invalid joins.
    Check that a given field exists before attempting to join it onto a query.

--- Civi/Api4/Query/Api4SelectQuery.php
+++ Civi/Api4/Query/Api4SelectQuery.php
@@ -351,6 +351,7 @@ class Api4SelectQuery extends SelectQuery {
 
   /**
    * @param $key
+   * @throws \API_Exception
    */
   protected function joinFK($key) {
     $pathArray = explode('.', $key);
@@ -383,6 +384,10 @@ class Api4SelectQuery extends SelectQuery {
       foreach ($lastLink->getEntityFields() as $fieldObject) {
         $this->apiFieldSpec[$prefix . $fieldObject->getName()] = $fieldObject->toArray() + ['entity' => $joinEntity];
       }
+    }
+
+    if (!$lastLink->getField($field)) {
+      throw new \API_Exception('Invalid join');
     }
 
     // custom groups use aliases for field names
