dd66816486aa5fe5799ad3f06ab006f1464d1175
Coleman Watts <coleman@civicrm.org>
2019-05-30 21:29:24 -0400

    Improve Event api

--- Civi/Api4/Event.php
+++ Civi/Api4/Event.php
@@ -9,4 +9,11 @@ namespace Civi\Api4;
  */
 class Event extends Generic\DAOEntity {
 
+  /**
+   * @return \Civi\Api4\Action\Event\Create
+   */
+  public static function create() {
+    return new \Civi\Api4\Action\Event\Create(__CLASS__, __FUNCTION__);
+  }
+
 }
