0f8f8f5c01e97bc1f189468b589511fd7a6a5747
Coleman Watts <coleman@civicrm.org>
2019-06-14 07:48:27 -0400

    Fix contribution update

--- Civi/Api4/Contribution.php
+++ Civi/Api4/Contribution.php
@@ -9,4 +9,18 @@ namespace Civi\Api4;
  */
 class Contribution extends Generic\DAOEntity {
 
+  /**
+   * @return \Civi\Api4\Action\Contribution\Create
+   */
+  public static function create() {
+    return new \Civi\Api4\Action\Contribution\Create(__CLASS__, __FUNCTION__);
+  }
+
+  /**
+   * @return \Civi\Api4\Action\Contribution\Update
+   */
+  public static function update() {
+    return new \Civi\Api4\Action\Contribution\Update(__CLASS__, __FUNCTION__);
+  }
+
 }
