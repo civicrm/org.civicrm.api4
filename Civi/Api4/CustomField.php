3b910c27ec6c7a76936e3f8d1e30b44219eaa10a
Coleman Watts <coleman@civicrm.org>
2019-07-16 07:34:40 -0400

    Use preSaveSubscriber for customField creation

--- Civi/Api4/CustomField.php
+++ Civi/Api4/CustomField.php
@@ -9,11 +9,4 @@ namespace Civi\Api4;
  */
 class CustomField extends Generic\DAOEntity {
 
-  /**
-   * @return \Civi\Api4\Action\CustomField\Create
-   */
-  public static function create() {
-    return new \Civi\Api4\Action\CustomField\Create(__CLASS__, __FUNCTION__);
-  }
-
 }
