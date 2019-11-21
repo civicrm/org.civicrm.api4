689a5ef3ba0115a1194e3d7dd7fa36eae3919491
Tim Otten <totten@civicrm.org>
2019-11-20 16:55:21 -0800

    Bump version to 4.4.4

--- api4.php
+++ api4.php
@@ -200,7 +200,7 @@ function api4_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
  * Implements hook_civicrm_check().
  */
 function api4_civicrm_check(&$messages) {
-  // Upgrading APIv4 @4.4.3+ without also upgrading core will make things worse.
+  // Upgrading APIv4 @4.4.4+ without also upgrading core will make things worse.
   // It's fine to upgrade them in whatever order - you just need to do it.
   $ver = CRM_Utils_System::version();
   $hasBaoApiKeyEnforcement =
@@ -210,7 +210,7 @@ function api4_civicrm_check(&$messages) {
   if (!$hasBaoApiKeyEnforcement) {
     $messages[] = new CRM_Utils_Check_Message(
       'api4_insecure',
-      ts('The APIv4 extension (v4.4.3+) relies on the security updates of 20-Nov-19. Update to CiviCRM 5.13.7+ or 5.19.2+. (Or, if you cannot, then downgrade to extension v4.4.2).'),
+      ts('The APIv4 extension (v4.4.4+) relies on the security updates of 20-Nov-19. Update to CiviCRM 5.13.7+ or 5.19.2+. (Or, if you cannot, then downgrade to extension v4.4.2).'),
       ts('APIv4 Security Requirement'),
       \Psr\Log\LogLevel::WARNING,
       'fa-flag'
